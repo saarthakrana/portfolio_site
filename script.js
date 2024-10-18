06dfca979ea743bf25dbc6336515591d89ef35e1
saarthakrana <91086362+saarthakrana@users.noreply.github.com>
2024-10-18 11:39:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let menu = document.getElementById('menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backend Developer','Certified Racist'],
+    typeSpeed:80,
+    backSpeed:80,
+    backDelay: 1200,
+    loop:true,
+});
+
